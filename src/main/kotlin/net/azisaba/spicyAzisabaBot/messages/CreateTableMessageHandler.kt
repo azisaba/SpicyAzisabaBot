857eae244e4bd0ebdc0ff01ee974f761c8d3401e
acrylic-style <me@acrylicstyle.xyz>
2022-02-21 13:43:28 +0900

    why is placeholder not supported for table name

--- src/main/kotlin/net/azisaba/spicyAzisabaBot/messages/CreateTableMessageHandler.kt
+++ src/main/kotlin/net/azisaba/spicyAzisabaBot/messages/CreateTableMessageHandler.kt
@@ -21,26 +21,28 @@ object CreateTableMessageHandler: MessageHandler {
             return
         }
         Util.getConnection().use {
-            val statement = it.prepareStatement("""
-                CREATE TABLE ? (
-                    `guild_id` VARCHAR(255) NOT NULL DEFAULT "0", -- guild (server) id
-                    `guild_name` VARCHAR(255) NOT NULL DEFAULT "0", -- guild (server) name
-                    `channel_id` VARCHAR(255) NOT NULL DEFAULT "0", -- channel id
-                    `channel_name` VARCHAR(255) NOT NULL DEFAULT "unknown", -- channel name
-                    `author_is_bot` VARCHAR(255) NOT NULL DEFAULT "0", -- user id of author [5]
-                    `author_id` VARCHAR(255) NOT NULL DEFAULT "0", -- user id of author
-                    `author_name` VARCHAR(255) NOT NULL DEFAULT "Deleted User", -- "username"#0000
-                    `author_discriminator` VARCHAR(16) NOT NULL DEFAULT "0000", -- username#"0000"
-                    `content` TEXT(65535) NOT NULL DEFAULT "", -- message content
-                    `edited` BOOLEAN NOT NULL DEFAULT FALSE, -- true if edited at least once [10]
-                    `edited_timestamp` BIGINT(255) NOT NULL DEFAULT 0, -- timestamp when the message was last edited at
-                    `created_timestamp` BIGINT(255) NOT NULL DEFAULT 0, -- timestamp when the message was created at
-                    `is_reply` BOOLEAN NOT NULL DEFAULT FALSE, -- true if reply, false otherwise
-                    `reply_to` VARCHAR(255) DEFAULT NULL -- referenced message id [14]
+            it.createStatement().use { statement ->
+                statement.executeUpdate(
+                    """
+                    CREATE TABLE ${args[1]} (
+                        `guild_id` VARCHAR(255) NOT NULL DEFAULT "0", -- guild (server) id
+                        `guild_name` VARCHAR(255) NOT NULL DEFAULT "0", -- guild (server) name
+                        `channel_id` VARCHAR(255) NOT NULL DEFAULT "0", -- channel id
+                        `channel_name` VARCHAR(255) NOT NULL DEFAULT "unknown", -- channel name
+                        `author_is_bot` VARCHAR(255) NOT NULL DEFAULT "0", -- user id of author [5]
+                        `author_id` VARCHAR(255) NOT NULL DEFAULT "0", -- user id of author
+                        `author_name` VARCHAR(255) NOT NULL DEFAULT "Deleted User", -- "username"#0000
+                        `author_discriminator` VARCHAR(16) NOT NULL DEFAULT "0000", -- username#"0000"
+                        `content` TEXT(65535) NOT NULL DEFAULT "", -- message content
+                        `edited` BOOLEAN NOT NULL DEFAULT FALSE, -- true if edited at least once [10]
+                        `edited_timestamp` BIGINT(255) NOT NULL DEFAULT 0, -- timestamp when the message was last edited at
+                        `created_timestamp` BIGINT(255) NOT NULL DEFAULT 0, -- timestamp when the message was created at
+                        `is_reply` BOOLEAN NOT NULL DEFAULT FALSE, -- true if reply, false otherwise
+                        `reply_to` VARCHAR(255) DEFAULT NULL -- referenced message id [14]
+                    )
+                    """.trimIndent()
                 )
-            """.trimIndent())
-            statement.setObject(1, args[1])
-            statement.executeUpdate()
+            }
         }
         message.reply { content = "テーブル`${args[1]}`を作成しました。" }
     }
