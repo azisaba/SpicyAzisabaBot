d52315c45bd52779af529289cc0ac8ae8a8d55cd
acrylic-style <me@acrylicstyle.xyz>
2022-04-22 11:28:12 +0900

    more env vars

--- src/main/kotlin/net/azisaba/spicyAzisabaBot/util/Constant.kt
+++ src/main/kotlin/net/azisaba/spicyAzisabaBot/util/Constant.kt
@@ -3,6 +3,6 @@ package net.azisaba.spicyAzisabaBot.util
 import dev.kord.common.entity.Snowflake
 
 object Constant {
-    val REAL_PROBLEM_CHANNEL_ID = Snowflake(760438206847123497L)
-    val DEVELOPER_ROLE = Snowflake(695920999803125781)
+    val REAL_PROBLEM_CHANNEL_ID = Snowflake(System.getenv("REAL_PROBLEM_CHANNEL_ID")?.toLongOrNull() ?: 760438206847123497L)
+    val DEVELOPER_ROLE = Snowflake(System.getenv("DEVELOPER_ROLE_ID")?.toLongOrNull() ?: 695920999803125781L)
 }
