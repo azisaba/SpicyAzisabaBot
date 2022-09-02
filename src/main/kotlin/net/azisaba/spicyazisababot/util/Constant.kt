9e354b88d8ed06aef723a43847636c0f4ba1d38b
acrylic-style <me@acrylicstyle.xyz>
2022-09-02 12:35:26 +0900

    Remove DEVELOPER_ROLE_ID and BUILDER_ROLE_ID

--- src/main/kotlin/net/azisaba/spicyazisababot/util/Constant.kt
+++ src/main/kotlin/net/azisaba/spicyazisababot/util/Constant.kt
@@ -4,8 +4,6 @@ import dev.kord.common.entity.Snowflake
 
 object Constant {
     val REAL_PROBLEM_CHANNEL_ID = Snowflake(System.getenv("REAL_PROBLEM_CHANNEL_ID")?.toLongOrNull() ?: 760438206847123497L)
-    val DEVELOPER_ROLE = Snowflake(System.getenv("DEVELOPER_ROLE_ID")?.toLongOrNull() ?: 695920999803125781L)
-    val BUILDER_ROLE = Snowflake(System.getenv("BUILDER_ROLE_ID")?.toLongOrNull() ?: 0L)
     val SELF_INTRO_CHANNEL = System.getenv("SELF_INTRO_CHANNEL") ?: "720660766952390696"
     val LEAVE_LOG_CHANNEL = Snowflake(System.getenv("LEAVE_LOG_CHANNEL")?.toLongOrNull() ?: 984491166466719774L)
     val MESSAGE_VIEWER_BASE_URL = System.getenv("MESSAGE_VIEWER_BASE_URL") ?: "https://messageviewer.azisaba.net"
