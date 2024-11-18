7a84ac9d0597193271ea99f94a0a0079c9dacd21
acrylic-style <me@acrylicstyle.xyz>
2024-11-18 21:54:23 +0900

    abcdef

--- src/main/kotlin/net/azisaba/spicyazisababot/commands/ToDBCommand.kt
+++ src/main/kotlin/net/azisaba/spicyazisababot/commands/ToDBCommand.kt
@@ -36,6 +36,31 @@ object ToDBCommand : CommandHandler {
 
     override suspend fun handle0(interaction: ApplicationCommandInteraction) {
         val table = interaction.optString("table")!!
+        if (table == "abcdef") {
+            Util.getConnection().use { connection ->
+                connection.prepareStatement("SELECT * FROM `attachments`").use { stmt ->
+                    stmt.executeQuery().use { rs ->
+                        while (rs.next()) {
+                            val messageId = rs.getString("message_id")
+                            val attachmentId = rs.getString("attachment_id")
+                            val url = rs.getString("url")
+                            val fileName = url.substringAfterLast("/")
+                            val data = rs.getBlob("data")
+                            println("Converting attachment $messageId-$attachmentId-$fileName")
+                            Util.uploadAttachment("$messageId-$attachmentId-$fileName", data.binaryStream)
+                            connection.prepareStatement("UPDATE `attachments` SET `data` = NULL, `url` = ? WHERE `message_id` = ? AND `attachment_id` = ?").use { updateStmt ->
+                                updateStmt.setString(1, "${BotConfig.config.attachmentsRootUrl}/$messageId-$attachmentId-$fileName")
+                                updateStmt.setString(2, messageId)
+                                updateStmt.setString(3, attachmentId)
+                                updateStmt.executeUpdate()
+                            }
+                            println("Converted attachment $messageId-$attachmentId-$fileName")
+                        }
+                    }
+                }
+            }
+            return
+        }
         if (!table.validateTable()) {
             interaction.respondEphemeral { content = "Invalid table name" }
             return
